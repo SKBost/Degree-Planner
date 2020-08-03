5ea959892425ab60e19d78bce366df2aacf5a195
Aven Huang <xih017@ucsd.edu>
2020-08-03 11:05:27 +0800

    untested Schedule class

--- app/src/main/java/com/example/degreeplanner/classes/Schedule.java
+++ app/src/main/java/com/example/degreeplanner/classes/Schedule.java
@@ -2,6 +2,7 @@ package com.example.degreeplanner.classes;
 
 import java.util.ArrayList;
 
+// untested
 public class Schedule { // Notes: refer to the structure of quarter-by-quarter worksheet when considering the fields.
     // quarter list for the entire schedule
     private ArrayList<Quarter> quarters;
