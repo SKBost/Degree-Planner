117070791a5d8c901048ff0c2c361873045e46ee
aneesha <anramasw@ucsd.edu>
2020-08-02 20:34:03 -0700

    Added more button functionality

--- app/src/main/java/com/example/degreeplanner/ui/resources/CollegeAdvising.java
+++ app/src/main/java/com/example/degreeplanner/ui/resources/CollegeAdvising.java
@@ -3,6 +3,8 @@ package com.example.degreeplanner.ui.resources;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.degreeplanner.R;
 
@@ -12,5 +14,14 @@ public class CollegeAdvising extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_college_advising);
+
+        // Close page once button is pressed
+        ImageButton closeButton = findViewById(R.id.college_back_button);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
     }
 }
