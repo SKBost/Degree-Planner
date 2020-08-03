5677ecb6564e2fe4244b37496fbc8c538431c6e6
Aven Huang <xih017@ucsd.edu>
2020-08-03 10:55:52 +0800

    create Schedule class

--- app/src/main/java/com/example/degreeplanner/classes/Schedule.java
+++ app/src/main/java/com/example/degreeplanner/classes/Schedule.java
@@ -1,30 +1,30 @@
-example.degreeplanner.classes;
+package com.example.degreeplanner.classes;
 
 import java.util.ArrayList;
 
 public class Schedule { // Notes: refer to the structure of quarter-by-quarter worksheet when considering the fields.
     // quarter list for the entire schedule
-    private ArrayList<com.example.degreeplanner.classes.Quarter> quarters;
+    private ArrayList<Quarter> quarters;
     // total units for the entire schedule
     private double totalUnits;
 
     // minimal constructor
     public Schedule() {
-        quarters = new ArrayList<com.example.degreeplanner.classes.Quarter>();
+        quarters = new ArrayList<>();
     }
 
     // maximal constructor
-    public Schedule(ArrayList<com.example.degreeplanner.classes.Quarter> myQuarters, double myTotalUnits) {
-        quarters = new ArrayList<com.example.degreeplanner.classes.Quarter>(myQuarters);
+    public Schedule(ArrayList<Quarter> myQuarters, double myTotalUnits) {
+        quarters = new ArrayList<>(myQuarters);
         totalUnits = myTotalUnits;
     }
 
     // getters
-    public ArrayList<com.example.degreeplanner.classes.Quarter> getQuarters() { return quarters;}
+    public ArrayList<Quarter> getQuarters() { return quarters;}
     public double getTotalUnits() { return totalUnits;}
 
     // setters
-    public void setQuarters(ArrayList<com.example.degreeplanner.classes.Quarter> newQuarters) { quarters = newQuarters;}
+    public void setQuarters(ArrayList<Quarter> newQuarters) { quarters = newQuarters;}
     public void setTotalUnits(double newTotalUnits) { totalUnits = newTotalUnits;}
 
     // possible TODO: checklist of AHI, DEI, etc,.
