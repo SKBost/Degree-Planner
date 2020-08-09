d5230e527d21cd6501e2084b7be56fa7357947d0
Aven Huang <xih017@ucsd.edu>
2020-08-09 22:52:54 +0800

    getting user inputs for adding requirements

--- app/src/main/java/com/example/degreeplanner/ui/requirements/AddRequirement.java
+++ app/src/main/java/com/example/degreeplanner/ui/requirements/AddRequirement.java
@@ -1,15 +1,14 @@
 package com.example.degreeplanner.ui.requirements;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.degreeplanner.R;
-import com.example.degreeplanner.activities.MainActivity;
 
 public class AddRequirement extends AppCompatActivity {
 
@@ -26,7 +25,31 @@ public class AddRequirement extends AppCompatActivity {
                 finish();
             }
         });
+
+        final EditText department = (EditText) findViewById(R.id.text_department);
+        final EditText course_code = (EditText) findViewById(R.id.text_course_code);
+        final EditText units = (EditText) findViewById(R.id.number_units);
+
+
+        Button saveReq = (Button) findViewById(R.id.save_button);
+        saveReq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // get user inputs for the requirements
+                String department_text = department.getText().toString();
+                String course_code_text = course_code.getText().toString();
+                String units_text = units.getText().toString();
+
+                /*
+                * Get all the input fields
+                * Write them using an API to your database
+                * Use Http handlers to write to the database
+                * */
+            }
+        });
 /*
+
         // Close and save page once button is pressed
         Button saveButton = findViewById(R.id.save_button);
         closeButton.setOnClickListener(new View.OnClickListener() {
