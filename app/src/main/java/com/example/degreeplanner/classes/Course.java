d8f0f42281d1b6e4d2b13400207de3f6c3f4e028
SKBost <ebost@ucsd.edu>
2020-08-20 00:30:04 -0700

    added first prereq functions and fixed compilation error

--- app/src/main/java/com/example/degreeplanner/classes/Course.java
+++ app/src/main/java/com/example/degreeplanner/classes/Course.java
@@ -2,6 +2,9 @@ package com.example.degreeplanner.classes;
 
 import com.example.degreeplanner.enums.GradingOption;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Course { //NOTES: test base class; TODO: need to add prereqs class and related method implemntations
 
     private String dept; // ex: COGS oe CSE
@@ -10,7 +13,7 @@ public class Course { //NOTES: test base class; TODO: need to add prereqs class 
     private GradingOption option; // LETTER, PNP, or UNCOUNTED
     private String notes;
     // TODO: PRE-REQS (note: do not change pre-reqs constructor signatures to add pre-reqs; if you want, add a new constructor instead)
-    //private ArrayList<Prerequisite> prereqs; //note: prereq class does not exist yet
+    private ArrayList<Course> prereqs; //note: prereq class does not exist yet; using course class for simple "and" operations
 
     // constructors
 
@@ -21,16 +24,30 @@ public class Course { //NOTES: test base class; TODO: need to add prereqs class 
         option = GradingOption.UNCOUNTED;
         units = 0;
         notes = "";
+        prereqs = new ArrayList<Course>();
     }
 
-    // full constructor (if needed)
+    // almost full constructor (if needed)
     public Course(String myDept, String myCode, GradingOption myOption, double myUnits, String myNotes) {
         dept = myDept;
         code = myCode;
         option = myOption;
         units = myUnits;
         notes = myNotes;
+        prereqs = new ArrayList<Course>();
     }
+
+    // full constructor (if needed)
+    public Course(String myDept, String myCode, GradingOption myOption, double myUnits, String myNotes, ArrayList<Course> myPrereqs) {
+        dept = myDept;
+        code = myCode;
+        option = myOption;
+        units = myUnits;
+        notes = myNotes;
+        prereqs = myPrereqs;
+    }
+
+
 
     // getters (note: packed together because they are trivial)
     public String getDept() { return dept; }
@@ -38,7 +55,7 @@ public class Course { //NOTES: test base class; TODO: need to add prereqs class 
     public double getUnits() { return units; }
     public GradingOption getOption() { return option; }
     public String getNotes() { return notes; }
-    //public ArrayList<Prerequisite> getPrereqs() { return prereqs; }
+    public ArrayList<Course> getPrereqs() { return prereqs; }
 
     // setters (note: packed together because they are trivial)
     public void setDept(String newDept) { dept = newDept; }
@@ -46,14 +63,28 @@ public class Course { //NOTES: test base class; TODO: need to add prereqs class 
     public void setUnits(double newUnits) { units = newUnits; }  //TODO: maybe check for invalid unit input?
     public void setOption(GradingOption newOption) { option = newOption; }
     public void setNotes(String newNotes) { notes = newNotes; }
-    //public void setPrereqs(ArrayList<Prerequisite> newPrereqs) { prereqs = newPrereqs; }
+    public void setPrereqs(ArrayList<Course> newPrereqs) { prereqs = newPrereqs; }
+
+    // other
 
     /* templates:
     public x getx() { return x; }
     public void setx(x newx) { x = newx; }
      */
 
-    // likely todo: method for adding/removing prereqs
+    // likely todo: method for removing prereqs
+    public void addPrereq(Course newPrereq) {
+        prereqs.add(newPrereq);
+    }
+
+    public void removePrereq(Course removed) {
+        for(int i = 0; i < prereqs.size(); i++) {
+            if(removed.sameCourseNameAs(prereqs.get(i))) {
+                prereqs.remove(i);
+                break;
+            }
+        }
+    }
 
     // compares if two courses are the same
     public boolean compareCourses(Course myCourse) {
@@ -62,6 +93,14 @@ public class Course { //NOTES: test base class; TODO: need to add prereqs class 
             if(option == myCourse.option) {
                 return true;
             }
+        }
+        return false;
+    }
+
+    // tests if dept and code are the same
+    public boolean sameCourseNameAs(Course myCourse) { // do not change this.
+        if(this.dept.compareTo(myCourse.dept) == 0 && this.code.compareTo(myCourse.code) == 0) {
+            return true;
         }
         return false;
     }
